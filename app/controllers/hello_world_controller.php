6a882598717ceb5d7d59a395d11d0bf52cd0ec85
Laura <laura.kaurijoki@helsinki.fi>
2016-12-27 02:15:14 +0200

    Lisätty staattiset näkymät

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,23 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+        View::make('suunnitelmat/home.html');
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+      View::make('helloworld.html');
+    }
+    
+    public static function course_list(){
+        View::make('suunnitelmat/course_list.html');
+    }
+    
+    public static function course_show(){
+        View::make('suunnitelmat/course_show.html');
+    }
+    
+    public static function course_edit(){
+        View::make('suunnitelmat/course_edit.html');
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,15 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+  
+  $routes->get('/course', function() {
+    HelloWorldController::course_list();
+  });
+  
+  $routes->get('/course/1', function() {
+    HelloWorldController::course_show();
+  });
+  
+  $routes->get('/course/1/edit', function() {
+    HelloWorldController::course_edit();
+  });
