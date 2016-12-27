473a16ae330445c9fbb4190790bbe8d00921c2a8
Laura <laura.kaurijoki@helsinki.fi>
2016-12-27 11:44:00 +0200

    Lisätty tietokantataulut sekä testidataa

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,7 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Kurssi (nimi, yliopisto) VALUES ('Logiikka I', 'Helsingin yliopisto');
+INSERT INTO Kurssi (nimi, yliopisto) VALUES ('Ohjelmoinnin perusteet', 'Helsingin yliopisto');
+INSERT INTO Kurssi (nimi, yliopisto) VALUES ('C-ohjelmointi', 'Aalto-yliopisto');
+
+INSERT INTO Opiskelija (nimi, kayttajatunnus, salasana) VALUES('Olli Opiskelija', 'Ollioo', 'sala123');
+
+INSERT INTO Muistiinpano (aihe, osoite) VALUES('1. luento', 'https://docs.google.com/document/d/184xmF0zNM0GBJeOgcdJJ0PJjfGcDoHy-PyBY35NkYEY/edit?usp=sharing');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,27 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Kurssi(
+    kurssitunnus SERIAL PRIMARY KEY,
+    nimi varchar(150),
+    yliopisto varchar(150) NOT NULL
+);
+
+CREATE TABLE Opiskelija(
+    opiskelijatunnus SERIAL PRIMARY KEY,
+    nimi varchar(100) NOT NULL,
+    kayttajatunnus varchar(50) NOT NULL,
+    salasana varchar(50) NOT NULL
+);
+
+CREATE TABLE Opiskelijan_kurssi(
+    opiskelija INTEGER REFERENCES Opiskelija(opiskelijatunnus),
+    kurssi INTEGER REFERENCES Kurssi(kurssitunnus)
+);
+
+CREATE TABLE Muistiinpano(
+    id SERIAL PRIMARY KEY,
+    opiskelija INTEGER REFERENCES Opiskelija(opiskelijatunnus),
+    kurssi INTEGER REFERENCES Kurssi(kurssitunnus),
+    aihe Varchar(200),
+    osoite Varchar(512) NOT NULL,
+    muokkauspaiva DATE,
+    julkinen boolean DEFAULT FALSE
+);
